a943e950474feabc1d98125c77c1fd06b13b0de5
David Brunow <helloDavid@brunow.org>
2016-04-10 17:44:58 -0500

    Class 2

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,11 +12,58 @@ class ViewController: UIViewController
 {
     @IBOutlet weak var display: UILabel!
     
+    var isInTheMiddleOfTypingANumber: Bool = false
+    
     @IBAction func appendDigit(sender: UIButton)
     {
-        let digit = sender.currentTitle
+        let digit = sender.currentTitle!
         
+        if isInTheMiddleOfTypingANumber
+        {
+            display.text = display.text! + digit
+        }
+        else
+        {
+            display.text = digit
+            isInTheMiddleOfTypingANumber = true
+        }
         print("Append this digit: \(digit)")
     }
+    
+    var operandStack = Array<Double>()
+    
+    var displayValue: Double
+    {
+        get
+        {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set
+        {
+            display.text = "\(newValue)"
+        }
+    }
+    
+    @IBAction func enter()
+    {
+        isInTheMiddleOfTypingANumber = false
+        
+        operandStack.append(displayValue)
+        print("Operand Stack: \(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton)
+    {
+        let operation = sender.currentTitle!
+        
+        switch operation
+        {
+         case "÷":
+            break
+        default:
+            break
+        }
+    }
+    
 }
 
